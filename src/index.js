7f7c6c13c4dfebd972c4235ac593ebbddf2dbbdd
LeanaRain <137782467+LeanaRain@users.noreply.github.com>
2024-09-23 00:45:15 +0500

    init: start task and try stack

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const stack = []; //исп. stack, врем. хранение данных
+  for (let item of str) {
+    let matchFound = false; // соответствие найдено - по умолч. фолс.
+    //проверкк каждой пары на соответствие
+    for(let[key, value] of bracketsKonfig) {
+      if (item === key) {
+        stack.push(item); //если это ключ добав его на вершину стека
+        matchFound = true;
+        break;
+      } else if (item === value) {
+        //если это значение то проверяем верхний эл-т стека
+        const last = stack.pop(); //pop - удаление эл-та с верш стека
+        if (last === undefined || last !== key) {
+          return false; // если в стеке не было эл-та или он не равен ключу
+        }
+        matchFound = true;
+      }
+    }
+  }
+
 }
