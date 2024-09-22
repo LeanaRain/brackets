b50d5fa037da5414d22180c1ef75811ba000dae9
LeanaRain <137782467+LeanaRain@users.noreply.github.com>
2024-09-23 01:11:30 +0500

    feat: add check of same brackets

--- src/index.js
+++ src/index.js
@@ -5,14 +5,22 @@ module.exports = function check(str, bracketsConfig) {
     //проверкк каждой пары на соответствие
     for(let[key, value] of bracketsConfig) {
       if (item === key) {
-        stack.push(item); //если это ключ добав его на вершину стека
+        //добав. проверка если символы одинаковые
+        if (key === value && stack[stack.length - 1] === key) {
+          stack.pop();
+        } else {
+          stack.push(item); //если это ключ добав его на вершину стека
+        }
         matchFound = true;
         break;
       } else if (item === value) {
         //если это значение то проверяем верхний эл-т стека
         const last = stack.pop(); //pop - удаление эл-та с верш стека
-        if (last === undefined || last !== key) {
+        if (last === undefined || key === value && last !== key) { // добав. проверка если символы одинаковы
           return false; // если в стеке не было эл-та или он не равен ключу
+        } else if (last !== key) {
+          stack.push(last);
+          return false; // если пара не совпала
         }
         matchFound = true;
         break;
